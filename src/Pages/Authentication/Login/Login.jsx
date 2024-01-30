bf4fc6bd6acff02843061139648f406969ce8242
78faysal <faysal7830@gmail.com>
2024-01-30 23:17:58 +0600

    register and login form created

---
+++ src/Pages/Authentication/Login/Login.jsx
@@ -0,0 +1,80 @@
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div>
+      <section className="bg-gray-50 dark:bg-gray-900">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-center text-gray-900 md:text-2xl dark:text-white">
+                Sign in to Talent Trail!
+              </h1>
+              <form className="space-y-4 md:space-y-6">
+                <div>
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Your email"
+                    required
+                  />
+                </div>
+                <div>
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                  />
+                </div>
+                <div className="flex items-center justify-between">
+                  <div className="flex items-start">
+                    <div className="flex items-center h-5">
+                      <input
+                        type="checkbox"
+                        className="checkbox checkbox-info checkbox-xs"
+                      />
+                    </div>
+                    <div className="ml-3 text-sm">
+                      <label className="text-gray-500 dark:text-gray-300">
+                        Remember me
+                      </label>
+                    </div>
+                  </div>
+                  <Link
+                    href=""
+                    className="text-sm text-primary hover:underline dark:text-primary"
+                  >
+                    Forgot password?
+                  </Link>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full text-white border bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Sign in
+                </button>
+                <p className="text-sm font-light text-center text-gray-500 dark:text-gray-400">
+                  Don’t have an account yet?{" "}
+                  <Link className="text-primary" to={"/register"}>
+                    Sign Up
+                  </Link>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Login;
